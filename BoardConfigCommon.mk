74146820b94f6b43b52160653560f51c4138dc5c
Hamster Tian <haotia@gmail.com>
2018-12-02 12:49:27 +0800

    yoshino: declare HDR display
    Way ahead of AMOLED on OnePlus phones.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -198,6 +198,8 @@ OVERRIDE_RS_DRIVER:= libRSDriver_adreno.so
 VSYNC_EVENT_PHASE_OFFSET_NS := 2000000
 SF_VSYNC_EVENT_PHASE_OFFSET_NS := 6000000
 
+TARGET_HAS_HDR_DISPLAY := true
+
 # Enable dexpreopt to speed boot time
 ifeq ($(HOST_OS),linux)
   ifeq ($(call match-word-in-list,$(TARGET_BUILD_VARIANT),user),true)
