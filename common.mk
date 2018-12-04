30b48eb003da43192d8579393a93c3419bf3b6a7
Hamster Tian <haotia@gmail.com>
2018-12-04 21:16:37 +0800

    yoshino: use prebuilt GPS HAL
    Now that we have an update com.qualcomm.location.apk, we need
    proprietary GPS HAL to match it.
    
    And I don't know if the opensource version works with new modem.

--- common.mk
+++ common.mk
@@ -221,9 +221,7 @@ PRODUCT_PACKAGES += \
     libgnsspps \
     libgps.utils \
     liblocation_api \
-    libloc_api_v02 \
     libloc_core \
-    libloc_ds_api \
     libloc_pla \
     libvehiclenetwork-native
 
