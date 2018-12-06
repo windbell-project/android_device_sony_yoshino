2e99998afa033ce4ea481a68b0c0c4974a763b85
Hamster Tian <haotia@gmail.com>
2018-12-06 15:11:02 +0800

    yoshino: remove Android default HIDLs for already HIDL-ized services
    Default HIDLs are used to load traditional fingerprint.msm8998.so files.
    We have a bunch of updated HALs here, no need for traditional ones.

--- common.mk
+++ common.mk
@@ -198,7 +198,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service \
+    android.hardware.biometrics.fingerprint@2.1 \
     fingerprintd
 
 #PRODUCT_PACKAGES += \
